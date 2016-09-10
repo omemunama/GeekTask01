faf39a53696475be2c61f52f0b49728a5ce28e98
omemunama <xnursx@gmail.com>
2016-09-10 17:15:13 +0700

    add back icon on toolbar :
    MainActivity : setDisplayHomeAsUpEnabled

--- app/src/main/java/nursulaeman/geektask01/MainActivity.java
+++ app/src/main/java/nursulaeman/geektask01/MainActivity.java
@@ -2,6 +2,8 @@ package nursulaeman.geektask01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
     }
 }
