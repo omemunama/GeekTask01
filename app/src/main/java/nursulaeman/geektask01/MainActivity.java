e110c9975a10eeaeba523c87a6f24ba60504bd4b
omemunama <xnursx@gmail.com>
2016-09-10 21:03:48 +0700

    Add Image in toolbar

--- app/src/main/java/nursulaeman/geektask01/MainActivity.java
+++ app/src/main/java/nursulaeman/geektask01/MainActivity.java
@@ -3,6 +3,7 @@ package nursulaeman.geektask01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,4 +27,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
