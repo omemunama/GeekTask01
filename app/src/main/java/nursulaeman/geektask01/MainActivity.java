8b0e3e28cc61b5e9a9b8ddb84526e021c6b2384d
omemunama <xnursx@gmail.com>
2016-09-13 09:21:36 +0700

    button function with case, implements View.OnClickListener

--- app/src/main/java/nursulaeman/geektask01/MainActivity.java
+++ app/src/main/java/nursulaeman/geektask01/MainActivity.java
@@ -5,21 +5,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnC, btnB;
-    EditText editText1;
-    EditText editText2;
+    EditText editText1, editText2;
     TextView textView;
 
     String evalue, result;
@@ -70,6 +67,20 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView1);
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        fab.setOnClickListener(this);
+
         editText1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -86,152 +97,100 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "1");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "1");
                 }
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button2:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "2");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "2");
                 }
-
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button3:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "3");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "3");
                 }
-
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button4:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "4");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "4");
                 }
-
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button5:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "5");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "5");
                 }
-
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button6:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "6");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "6");
                 }
-
-            }
-        });
-
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button7:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "7");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "7");
                 }
-
-            }
-        });
-
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button8:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "8");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "8");
                 }
-
-            }
-        });
-
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button9:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "9");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "9");
                 }
-
-            }
-        });
-
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button0:
                 if (evalue == "1") {
                     editText1.setText(editText1.getText() + "0");
                 }
                 if (evalue == "2") {
                     editText2.setText(editText2.getText() + "0");
                 }
-
-            }
-        });
-
-        btnC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.buttonC:
                 if (evalue == "1") {
                     editText1.setText(" ");
                 }
                 if (evalue == "2") {
                     editText2.setText(" ");
                 }
-
-
-            }
-        });
-
-        btnB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.buttonBack:
                 if (evalue == "1") {
                     String str = editText1.getText().toString();
                     if (str.length() >= 1) {
@@ -250,12 +209,8 @@ public class MainActivity extends AppCompatActivity {
                         editText2.setText("");
                     }
                 }
-            }
-        });
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.fab:
                 if (TextUtils.isEmpty(editText1.getText().toString())
                         || TextUtils.isEmpty(editText2.getText().toString())) {
                     return;
@@ -267,9 +222,8 @@ public class MainActivity extends AppCompatActivity {
                     result = Float.toString(mBalance);
                     textView.setText(result);
                 }
-            }
-
-        });
+                break;
+        }
 
     }
 
